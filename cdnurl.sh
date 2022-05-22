63d933434ee0c7abbcf24d249a84a96bb71b43cb
seunghaekim <aka.seunghaekim@gmail.com>
2022-05-22 12:38:18 +0900

    build

--- cdnurl.sh
+++ cdnurl.sh
@@ -1,3 +1,4 @@
 __lastCommit=`git log --format="%H" -n 1`
+echo $__lastCommit
 echo "https://cdn.jsdelivr.net/gh/seunghaekim/annacaratt-ext@$__lastCommit/dist/app.js"
 echo "https://cdn.jsdelivr.net/gh/seunghaekim/annacaratt-ext@$__lastCommit/dist/app.css"
